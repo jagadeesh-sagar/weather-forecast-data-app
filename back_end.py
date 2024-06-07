8f9828c3bb45653cd08599ba61b3c321d6b74ec2
jagadeesh-sagar <nagarajug176@gmail.com>
2024-06-07 13:44:33 +0530

    Add sky conditions

--- back_end.py
+++ back_end.py
@@ -2,21 +2,16 @@ import requests
 import os
 API_KEY = os.getenv("weather_api")
 #cityname="Delhi"
-def get_data(place,forecast_days,kind):
+def get_data(place,forecast_days):
         url=f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
         response=requests.get(url)
         data=response.json()
         filter_data=data["list"]
         nr_values=8*forecast_days
         filter_data=filter_data[:nr_values]
-        if kind =="Temperature":
-                filter_data=[dict["main"]["temp"] for dict in filter_data]
-       
-        if kind=="Sky":
-                filter_data=[dict["weather"][0]["main"] for dict in filter_data]
         return filter_data
 
 
 if __name__=="__main__":
-    print(get_data(place="Hyderabad",forecast_days=3,kind="Temperature"))
+    get_data(place="london",forecast_days=3)
 
--- main.py
+++ main.py
@@ -3,12 +3,29 @@ import plotly.express as px
 from back_end import get_data
 
 st.title("Weather forecast for the Next Days")
-place=st.text_input("place:","Delhi")
+place=st.text_input("place:")
 days=st.slider("Forecast Days",min_value=1,max_value=5,help="select the number of forecasted days")
-option=st.selectbox("Select data to view",("Temperature","Sky"))
+option=st.selectbox("Select data to view",("Sky","Temperature"))
 st.subheader(f"Temperature for the next {days} days in {place}")
 
 
-data=get_data(place,days,option)
-figure=px.line(x=d,y=data,labels={"x":"Date","y":"Temperaturec(c)"})
-st.plotly_chart(figure)
+
+if place:
+    try:
+        filter_data = get_data(place, days)
+        if option == "Temperature":
+            temperature = [dict["main"]["temp"]/10 for dict in filter_data]
+
+            dates=[dict["dt_txt"] for dict in filter_data]
+            figure=px.line(x=dates,y=temperature,labels={"x":"Date","y":"Temperaturec(c)"})
+            st.plotly_chart(figure)
+        if option == "Sky":
+            images ={"Clear":"images (2)/Clear.png","Clouds":"images (2)/Clouds.png","Rain":"images (2)/rain.png","Snow":"images (2)/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filter_data]
+            images_paths=[images[condition] for condition in sky_conditions]
+
+            st.image(images_paths,width=115)
+
+    except (KeyError):
+        st.info("That place does not exit or not available")
+
